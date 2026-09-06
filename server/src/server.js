5e8286c441ccc613cee616a98b10b368a05c89a1
RockAndHand <rahulnishad143h@gmail.com>
2026-09-06 16:57:40 +0530

    chore: add root-level concurrently script, gitignore updates, add env templatates

--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,7 @@ const { Server } = require("socket.io");
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
   },
 });
 app.set("io", io);
@@ -47,7 +47,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT =  process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- syncly/src/socket.js
+++ syncly/src/socket.js
@@ -1,8 +1,9 @@
 import { io }
 from "socket.io-client";
 
+const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"
 const socket = io(
-  "http://localhost:5000"
+  SOCKET_URL
 );
 
 export default socket;
